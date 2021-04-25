0ee58347a7e8bdd1171eb78f28f8a54c3db16616
heemabhatt <heema.bhatt@gmail.com>
2021-04-25 11:39:18 -0400

    Update Helper.tsx

--- Helper.tsx
+++ Helper.tsx
@@ -1,39 +1,56 @@
+import { FontSizes, FontWeights, MessageBar, MessageBarType } from "@fluentui/react";
 import React = require("react");
-import { errorStyles } from "./style/Search-styles";
 
-export function isEmptyString(str?:string){
-    if(str === null || str ===undefined || str.length===0 ){
-        return true;
-    }
-    return false;
+export function isEmptyString(str?: string) {
+  if (str === null || str === undefined || str.length === 0) {
+    return true;
+  }
+  return false;
 }
 
-export function isNullObject(obj?:any)
-{
-    if(obj === null || obj === undefined)
-    {
-        return true;
-    }
-    return false;
+export function isNullObject(obj?: any) {
+  if (obj === null || obj === undefined) {
+    return true;
+  }
+  return false;
 }
 
-export function showError(error:string)
-{
-    console.log(`An error occurred: ${error}`);
-    alert(`An error occurred: ${error}`);
+export function showError(error: string) {
+  console.log(`An error occurred: ${error}`);
+ // alert(`An error occurred: ${error}`);
 }
 
-export function logInformation(message:any)
-{
-    console.log(message);
-   alert(message);
+export function logInformation(message: any) {
+  console.log(message);
+ // alert(message);
 }
 
 export function ErrorMessage(props: any) {
-    return (
-  //TODO: Style
-      <div id="CeoSearchError"   >
-        {props.message}
-      </div>
-    );
-  }  
+  const divStyle = {
+    color: "rgb(191, 9, 0)",
+    display:"flex",
+    FontSizes: FontSizes.size14,
+    FontWeights:FontWeights.regular,
+    marginRight:"auto"
+  };
+  return (
+    <MessageBar
+      messageBarType={MessageBarType.error}
+      isMultiline={false}
+      dismissButtonAriaLabel="Close" 
+      style={divStyle}  >
+      {props.message}
+
+    </MessageBar>
+
+  );
+}
+
+/*
+export function ErrorMessage(props: any) {
+return (
+  <div id="CeoSearchError" style={errorStyles}  >
+    {props.message}
+  </div>
+);
+}  */
