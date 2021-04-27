dab093ed9cf129c5bcdd3b41d7bd0d8efebea736
heemabhatt <heema.bhatt@gmail.com>
2021-04-27 11:25:26 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,10 +2,8 @@ import { IInputs, IOutputs } from "./generated/ManifestTypes";
 import ReactDOM = require("react-dom");
 import React = require("react");
 import { ISearchButtonProps, SearchButton } from "./Controls/SearchButton";
-import * as Helper from './Controls/Helper';
 export class CeoAuthenticationLogSearch implements ComponentFramework.StandardControl<IInputs, IOutputs> {
 	private _container: HTMLDivElement;
-
 	private _inputCeoUserId: string = "";
 	private _inputCeoCompanyId: string = "";
 	private _inputElement: React.ReactElement;
@@ -14,6 +12,7 @@ export class CeoAuthenticationLogSearch implements ComponentFramework.StandardCo
 	private searchProps: ISearchButtonProps;
 	private _outputCeoSearch: string;
 	constructor() {
+		this.onClickHandler = this.onClickHandler.bind(this);
 		this.searchProps = {
 			onClick: this.onClickHandler,
 			companyId: '',
@@ -24,11 +23,6 @@ export class CeoAuthenticationLogSearch implements ComponentFramework.StandardCo
 		}
 		this._outputCeoSearch = "";
 	}
-	public onClickHandler(event: Event) {
-
-		//console.log("Inside index.ts - onclickHandler: " + JSON.stringify(event));
-	}
-
 
 	public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container: HTMLDivElement) {
 		this._container = container;
@@ -38,7 +32,6 @@ export class CeoAuthenticationLogSearch implements ComponentFramework.StandardCo
 			&& (context.parameters.ceoCompanyId.raw !== null && context.parameters.ceoCompanyId.raw !== undefined)) {
 			this._inputCeoCompanyId = context.parameters.ceoCompanyId.raw || "";
 			this.searchProps.companyId = this._inputCeoCompanyId;
-
 		}
 		if ((context.parameters.ceoUserId !== null && context.parameters.ceoUserId !== undefined)
 			&& (context.parameters.ceoUserId.raw !== null && context.parameters.ceoUserId.raw !== undefined)) {
@@ -49,7 +42,6 @@ export class CeoAuthenticationLogSearch implements ComponentFramework.StandardCo
 			&& (context.parameters.ceoSearch.raw !== null && context.parameters.ceoSearch.raw !== undefined)) {
 			this._outputCeoSearch = context.parameters.ceoSearch.raw;
 			this.searchProps.ceoSearch = this._outputCeoSearch;
-			//	console.log("Inside index.ts - init: "+ JSON.stringify(this._outputCeoSearch));
 		}
 		this.searchProps.disabled = (this._inputCeoUserId.length === 0) || (this._inputCeoCompanyId.length === 0);
 	}
@@ -60,7 +52,6 @@ export class CeoAuthenticationLogSearch implements ComponentFramework.StandardCo
 			&& (context.parameters.ceoCompanyId.raw !== null && context.parameters.ceoCompanyId.raw !== undefined)) {
 			this._inputCeoCompanyId = context.parameters.ceoCompanyId.raw || "";
 			this.searchProps.companyId = this._inputCeoCompanyId;
-
 		}
 		if ((context.parameters.ceoUserId !== null && context.parameters.ceoUserId !== undefined)
 			&& (context.parameters.ceoUserId.raw !== null && context.parameters.ceoUserId.raw !== undefined)) {
@@ -72,22 +63,26 @@ export class CeoAuthenticationLogSearch implements ComponentFramework.StandardCo
 			&& (context.parameters.ceoSearch.raw !== null && context.parameters.ceoSearch.raw !== undefined)) {
 			this._outputCeoSearch = context.parameters.ceoSearch.raw;
 			this.searchProps.ceoSearch = this._outputCeoSearch;
-			console.log("Inside index.ts - update: " + JSON.stringify(context.parameters.ceoSearch.raw));
 		}
 
 		this.searchProps.disabled = (this._inputCeoUserId.length === 0) || (this._inputCeoCompanyId.length === 0);
 		this.searchProps.context = context;
 
 		this.renderControl(context);
-
 	}
 
 	private renderControl(context: ComponentFramework.Context<IInputs>) {
 		ReactDOM.render(this._inputElement = React.createElement(SearchButton, this.searchProps), this._container);
 	}
 
+	public onClickHandler(event: any) {
+		this._outputCeoSearch = JSON.stringify(event);
+		console.log("on Click Handler: " + this._outputCeoSearch);
+		this._notifyOutputChanged();
+	}
+
 	public getOutputs(): IOutputs {
-		//console.log("Inside index.ts - getOutputs: " + JSON.stringify(this._outputCeoSearch));
+		console.log("Returning Output... " + this._outputCeoSearch);
 		return {
 			ceoSearch: this._outputCeoSearch,
 			ceoUserId: this._inputCeoUserId, ceoCompanyId: this._inputCeoCompanyId
