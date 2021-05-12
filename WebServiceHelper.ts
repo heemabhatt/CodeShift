bd47a5c98e25890ef1af2c850af32c494c423c63
heemabhatt <heema.bhatt@gmail.com>
2021-05-11 21:56:10 -0400

    New fields mapping using SEAS and SEBS values

--- WebServiceHelper.ts
+++ WebServiceHelper.ts
@@ -1,58 +1,39 @@
-
-export async function callSeasService(companyId: string) {
-    console.log("Inside SEAS Service...");
-    if (companyId !== undefined && companyId.includes("company")) {
-        const seasResult = {
-            companyName: "SEAS-COMPANY" + Math.round(1000 * Math.random()).toString(),
-            address1_line1: "companyInfo.AddressLine1",
-            address1_city: "companyInfo.City",
-            address1_stateorprovince: "companyInfo.State",
-            address1_country: "companyInfo.Country",
-            address1_postalcode: "companyInfo.ZipCode",
-            wfsv_sorflag: false
-        };
+export async function callSeasService(companyId: string, context: any) {
+    const seasResultJson = "{\"CompanyName\":\"WQA CCER5 COMPANY B\",\"Address1Line1\":\"300 Park Ave. Suite 14001\",\"Address1City\":\"Minneapolis\",\"Address1State\":\"CA\",\"Address1Country\":\"US\",\"Address1ZipCode\":\"43555\"}";
+    const seasResult = JSON.parse(seasResultJson);
+    if(isValidSEASResult(seasResult))
+    {
+        console.log("SEAS RESULT inside function: " + JSON.stringify(seasResult));
         return seasResult;
     }
+    
+    return ""; 
 }
 
-export async function callSebsService(userId: string) {
-    console.log("Inside SEBS Service...");
-    let s1;
-    const sebsResult1 = {
-        firstname: "sebsFirstName",
-        lastname: "sebsLastName",
-        email: "sebsemail@sebs.com",
-        phonenumber: "121212",
-        mobilenumber: "123-456-7890"
-    };
-    if (userId !== undefined && userId.includes("user")) {
-        s1 = {
-            "@odata.context": "https://wfdeveloper0056.crm.dynamics.com/api/data/v9.1/$metadata#Microsoft.Dynamics.CRM.wfsv_CEOUserIdSearchResponse",
-            "ContactsInfo": "{\"ContactsOOPView\":[{\"AccountName\":\"WQA CCER5 COMPANY B\",\"Accountid\":\"\",\"BankID\":\"1838\",\"CEOCompanyId\":\"WQAC5B\",\"CEOUserId\":\"REA01401\",\"CompanyName\":\"WQA CCER5 COMPANY B\",\"ContactType\":\"External\",\"Contactid\":\"\",\"Email\":\"Derek.Byington@msn.ie\",\"FinancialAccountid\":\"\",\"FirstName\":\"An\",\"LastName\":\"Gunn\",\"Level1\":\"37474\",\"Level2\":\"\",\"Phone\":\"\",\"Role\":\"OOPView\",\"Status\":\"\"},{\"AccountName\":\"WQA CCER5 COMPANY B\",\"Accountid\":\"\",\"BankID\":\"1838\",\"CEOCompanyId\":\"WQAC5B\",\"CEOUserId\":\"REA01401\",\"CompanyName\":\"WQA CCER5 COMPANY B\",\"ContactType\":\"External\",\"Contactid\":\"\",\"Email\":\"Derek.Byington@msn.ie\",\"FinancialAccountid\":\"\",\"FirstName\":\"An\",\"LastName\":\"Gunn\",\"Level1\":\"37474\",\"Level2\":\"10035\",\"Phone\":\"\",\"Role\":\"OOPView\",\"Status\":\"\"},{\"AccountName\":\"WQA CCER5 COMPANY B\",\"Accountid\":\"\",\"BankID\":\"1838\",\"CEOCompanyId\":\"WQAC5B\",\"CEOUserId\":\"REA01401\",\"CompanyName\":\"WQA CCER5 COMPANY B\",\"ContactType\":\"External\",\"Contactid\":\"\",\"Email\":\"Derek.Byington@msn.ie\",\"FinancialAccountid\":\"\",\"FirstName\":\"An\",\"LastName\":\"Gunn\",\"Level1\":\"37474\",\"Level2\":\"37475\",\"Phone\":\"\",\"Role\":\"OOPView\",\"Status\":\"\"}]}"
-        };
-    }
-    else {
-        s1 = {
-            "@odata.context": "https://wfdeveloper0056.crm.dynamics.com/api/data/v9.1/$metadata#Microsoft.Dynamics.CRM.wfsv_CEOUserIdSearchResponse",
-            "ContactsInfo": "{\"ContactsOOPView\":[{\"AccountName\":null,\"Accountid\":\"\",\"BankID\":null,\"CEOCompanyId\":null,\"CEOUserId\":null,\"CompanyName\":null,\"ContactType\":\"External\",\"Contactid\":\"\",\"Email\":null,\"FinancialAccountid\":\"\",\"FirstName\":null,\"LastName\":null,\"Level1\":null,\"Level2\":null,\"Phone\":null,\"Role\":\"OOPView\",\"Status\":\"\"}]}"
-        };
-    }
-    const s2 = JSON.parse(s1.ContactsInfo);
-    if( s2 && s2.ContactsOOPView && s2.ContactsOOPView.length >0)
+export async function callSebsService(ceoCompanyId: string,ceoUserId: string, context: any) {
+    const sebsResultJson = "{\"FirstName\":\"Uesr\",\"MiddleInitial\":\"A\",\"LastName\":\"One\",\"FullName\":\"Uesr A One\",\"Email\":\"userone@wells.com\",\"PhoneNumber\":\"232344\",\"MobileNumber\":\"\",\"UserStatus\":\"ACTIVE\"}" ; 
+    const sebsResult = JSON.parse(sebsResultJson);
+    if(isValidSEBSResult(sebsResult))
     {
-        const sebsResult= s2.ContactsOOPView[0];
-        if(isValidSEBSResult(sebsResult))
-        {
-            console.log("SEBS RESULT inside function: " + JSON.stringify(sebsResult));
-            return sebsResult;
-        }
+        console.log("SEBS RESULT inside function: " + JSON.stringify(sebsResult));
+        return sebsResult;
     }
-    return "";
+    
+    return ""; 
 }
 
 export function isValidSEBSResult(sebsResult:any)
 {
-    if(sebsResult && sebsResult.AccountName && sebsResult.LastName)
+    if(sebsResult && sebsResult.LastName && sebsResult.FullName)//TODO: Add more mandatory fields in condition
+    {
+        return true;
+    }
+    return false;
+}
+
+export function isValidSEASResult(seasResult:any)
+{
+    if(seasResult && seasResult.CompanyName ) //TODO: Add more mandatory fields in condition
     {
         return true;
     }
